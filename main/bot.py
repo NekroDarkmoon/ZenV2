83a65a4965a8b63a3200ff680d3abe0643df87ca
NekroDarkmoon <junaid_maclaren97@hotmail.com>
2020-07-28 01:39:37 -0700

    Updated bot.py to work

--- main/bot.py
+++ main/bot.py
@@ -18,7 +18,7 @@ from discord.ext import commands
 # Local application imports
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_PATH)
-
+from cogs.utils import context # noqa
 
 # --------------------------------------------------------------------------
 #                                  Bot.py
@@ -90,7 +90,7 @@ def _prefix_callable(bot, msg):
 # --------------------------------------------------------------------------
 class Zen(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix=_prefix_callable, description=description, pm_help=None,
+        super().__init__(command_prefix='~', description=description, pm_help=None,
                          help_attrs=dict(hidden=True), fetch_offline_members=True,
                          heartbeat_timeout=150.0)
 
@@ -106,6 +106,25 @@ class Zen(commands.Bot):
 
         setup_logging()
         print('Logging setup Complete')
+
+    # Command Error handler
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.NoPrivateMessage):
+            await ctx.author.send('This command cannot be used in private messages.')
+        elif isinstance(error, commands.DisabledCommand):
+            await ctx.author.send('Sorry. This command is disabled and cannot be used.')
+        elif isinstance(error, commands.CommandInvokeError):
+            original = error.original
+            if not isinstance(original, discord.HTTPException):
+                print(f'In {ctx.command.qualified_name}:', file=sys.stderr)
+                traceback.print_tb(original.__traceback__)
+                print(f'{original.__class__.__name__}: {original}', file=sys.stderr)
+        elif isinstance(error, commands.ArgumentParsingError):
+            await ctx.send(error)
+
+    async def close(self):
+        await super().close()
+        await self.session.close()
 
 
 if __name__ == "__main__":
