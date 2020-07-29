d3c0e7d8a3e385eb6b3daaf0a582bcec0b53410a
NekroDarkmoon <junaid_maclaren97@hotmail.com>
2020-07-29 05:27:05 -0700

    Added ban, kick and unban

--- main/cogs/Admin.py
+++ main/cogs/Admin.py
@@ -15,6 +15,7 @@ from discord.ext import commands
 # Enabling local imports
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_PATH)
+from settings import embeds as emb # noqa
 
 
 # --------------------------------------------------------------------------
@@ -23,6 +24,68 @@ sys.path.append(BASE_PATH)
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    @commands.command(name="kick", pass_context=True)
+    @commands.guild_only()
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
+        """Kicks a member from the server."""
+
+        if reason is None:
+            reason = f"Kicked by {ctx.author}\nID: {ctx.author.id}"
+
+        e = emb.gen_embed_white(f'Kicked {member}', reason)
+        e.set_thumbnail(url=member.avatar_url)
+
+        try:
+            await ctx.guild.kick(member, reason=reason)
+            await ctx.message.delete()
+            await ctx.send(embed=e)
+        except Exception as e:
+            print(e)
+            embed = emb.gen_embed_orange('Error', e)
+            await ctx.send(embed=embed)
+
+    # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    @commands.command(name="ban", pass_context=True)
+    @commands.guild_only()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
+        """Bans a member from the server."""
+        if reason is None:
+            reason = f"Banned by {ctx.author}\nID: {ctx.author.id}"
+
+        e = emb.gen_embed_white(f'Banned {member}', reason)
+
+        try:
+            await ctx.guild.ban(member, reason=reason)
+            await ctx.message.delete()
+            await ctx.send(embed=e)
+        except Exception as e:
+            print(e)
+            embed = emb.gen_embed_orange('Error', e)
+            await ctx.send(embed=embed)
+
+    # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    @commands.command(name="unban", pass_context=True)
+    @commands.guild_only()
+    @commands.has_permissions(ban_members=True)
+    async def unban(self, ctx, member: discord.Member, *, reason=None):
+        """Unbans a member from the server"""
+        if reason is None:
+            reason = f"Unbanned by {ctx.author}.\nID: {ctx.author.id}"
+
+        e = emb.gen_embed_white(f'Unbanned {member}', reason)
+
+        try:
+            await ctx.guild.unban(member, reason=reason)
+            await ctx.message.delete()
+            await ctx.send(embed=e)
+        except Exception as e:
+            print(e)
+            embed = emb.gen_embed_orange('Error', e)
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
