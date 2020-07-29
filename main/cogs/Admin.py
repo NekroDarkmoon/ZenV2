ec785252a6cfa511139c0e4acd1d6641d0e9f730
NekroDarkmoon <junaid_maclaren97@hotmail.com>
2020-07-29 05:55:22 -0700

    Added userinfo

--- main/cogs/Admin.py
+++ main/cogs/Admin.py
@@ -87,6 +87,52 @@ class Admin(commands.Cog):
             embed = emb.gen_embed_orange('Error', e)
             await ctx.send(embed=embed)
 
+    # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    @commands.command(name="userinfo", pass_context=True)
+    @commands.guild_only()
+    async def userinfo(self, ctx, member: discord.Member):
+        """Displays information about a user"""
+        try:
+            # Variables
+            roles = [role.name.replace('@', '@\u200b') for role in getattr(member, 'roles', [])]
+            shared = sum(g.get_member(member.id) is not None for g in self.bot.guilds)
+
+            embed = discord.Embed()
+            embed.set_author(name=member)
+            embed.add_field(name='ID', value=member.id, inline=True)
+            embed.add_field(name='Servers', value=shared, inline=True)
+            embed.add_field(name='Joined', value=getattr(member, 'joined_at', None), inline=False)
+            embed.add_field(name='Created', value=member.created_at, inline=False)
+
+            voice = getattr(member, 'voice', None)
+            if voice is not None:
+                vc = voice.channel
+                other_people = len(vc.members) - 1
+                voice = f'{vc.name} with {other_people} others' if other_people else f'{vc.name} by themselves'
+                embed.add_field(name='Voice', value=voice, inline=False)
+
+            if roles:
+                embed.add_field(name='Roles', value=', '.join(roles) if len(roles) < 10 else
+                                f'{len(roles)} roles', inline=False)
+
+            color = member.colour
+            if color.value:
+                embed.color = color
+            else:
+                embed.color = 0xf2f6f7
+
+            if member.avatar:
+                embed.set_thumbnail(url=member.avatar_url)
+
+            if isinstance(member, discord.User):
+                embed.set_footer(text='This member is not in this server.')
+
+            await ctx.send(embed=embed)
+
+        except Exception as e:
+            print(e)
+            await ctx.send(embed=emb.gen_embed_orange('Error', e))
+
 
 def setup(bot):
     bot.add_cog(Admin(bot))
