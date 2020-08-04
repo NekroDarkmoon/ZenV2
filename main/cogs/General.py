126d093a5700862efea6a2feb8253a63fd802427
NekroDarkmoon <junaid_maclaren97@hotmail.com>
2020-08-04 10:21:19 -0700

    Added Weather Generator

--- main/cogs/General.py
+++ main/cogs/General.py
@@ -5,6 +5,8 @@
 # --------------------------------------------------------------------------
 # Standard library imports
 import logging
+import numpy as np
+import random
 import sys
 import os
 
@@ -48,6 +50,49 @@ class General(commands.Cog):
             await ctx.send(github + '\n' + trello)
         except Exception as e:
             print(e)
+
+    @commands.command(name='weather', help="Generates the weather for the world")
+    async def weather(self, ctx):
+        # Create embed
+        e = emb.gen_embed_cobalt('Weather Generator', None)
+
+        # Generate Precipitation
+        precip = ['No Precipitation', 'Light rain or snowfall', 'Heavy rain or snowfall']
+        precip = np.random.choice(precip, p=[0.6, 0.25, 0.15])
+        e.add_field(name='Precipitation', value=precip, inline=False)
+
+        # Generate Temps
+        roll = random.randint(1, 4)
+        temp = ['Normal for the season', f'{roll*10} degrees Fahrenheit colder than normal',
+                f'{roll*10} degrees Fahrenheit colder than normal']
+        temp = np.random.choice(temp, p=[0.7, 0.15, 0.15])
+        e.add_field(name='Temperature', value=temp, inline=False)
+
+        # Generate Wind
+        wind = ['No Wind', 'Light Wind', 'Medium Wind', 'Heavy/Strong Wind']
+        wind = np.random.choice(wind, p=[0.1, 0.55, 0.2, 0.15])
+        e.add_field(name='Wind', value=wind, inline=False)
+
+        # Generate speeds
+        roll = random.randint(1, 6) + random.randint(1, 6)
+        direction = ['North', 'North-East', 'East', 'South-East', 'South',
+                     'South-West', 'West', 'North-West']
+
+        if wind == 'Medium Wind':
+            speed = [50, 40, 30, 20, 10, 0, 0, 10, 20, 30, 40, 50]
+            speed = str(speed[roll-1]) + ' feet'
+            e.add_field(name='Speed', value=speed, inline=False)
+            direction = np.random.choice(direction)
+            e.add_field(name='Direction', value=direction, inline=False)
+
+        elif wind == 'Heavy/Strong Wind':
+            speed = [110, 100, 90, 80, 70, 60, 60, 70, 80, 90, 100, 110]
+            speed = str(speed[roll-1]) + ' feet'
+            e.add_field(name='Speed', value=speed, inline=True)
+            direction = np.random.choice(direction)
+            e.add_field(name='Direction', value=direction, inline=False)
+
+        await ctx.send(embed=e)
 
 
 def setup(bot):
