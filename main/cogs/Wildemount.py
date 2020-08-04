581ee86f9c665f0c92143e2cafcd384ba880e40e
NekroDarkmoon <junaid_maclaren97@hotmail.com>
2020-08-04 04:52:55 -0700

    Added clfg

--- main/cogs/Wildemount.py
+++ main/cogs/Wildemount.py
@@ -4,6 +4,7 @@
 #                                 Imports
 # --------------------------------------------------------------------------
 # Standard library imports
+import random
 import sys
 import os
 
@@ -29,11 +30,63 @@ class Wildemount(commands.Cog):
     async def cog_check(self, ctx):
         return ctx.guild.id == 739684323141353597
 
-    @commands.command(name="beep", pass_context="True")
-    async def beep(self, ctx):
+    # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    #                          Creating an lfg
+    @commands.command(name="clfg", help="Create a post for a game.",
+                      usage="d/p Description")
+    async def clfg(self, ctx, post_type, *, msg):
         await ctx.message.delete()
-        response = emb.gen_embed_green('Beep', 'Boop')
-        await ctx.send(embed=response)
+
+        # validation
+        if post_type.lower() == 'p':
+            post_type = "Player"
+        elif post_type.lower() == 'd':
+            post_type = "DM"
+        else:
+            response = emb.gen_embed_yellow('Error', 'Usage: &clfg p/d Description')
+            await ctx.send(embed=response)
+            return
+
+        if len(msg) < 5:
+            response = emb.gen_embed_yellow('Error', 'Description must be longer than 5 words.')
+            await ctx.send(embed=response)
+            return
+
+        # Getting variables
+        author = ctx.author.name + '#' + ctx.author.discriminator
+        post_id = random.randint(0, 10000000)
+        server_id = ctx.guild.id
+
+        # data_tuple = (server_id, post_id, author, post_type, msg)
+
+        sql = """INSERT INTO quest(server_id, quest_id, author, quest_type, msg)
+                 VALUES($1, $2, $3, $4, $5)"""
+
+        try:
+            await self.bot.pool.execute(sql, server_id, post_id, author, post_type, msg)
+            response = emb.gen_embed_green(
+                                        f'Looking for game - {post_id} ',
+                                        f'Entry successfully created.\n\n{msg}')
+            await ctx.send(embed=response)
+        except Exception as e:
+            print("Failed to enter data", e)
+            await ctx.send(embed=emb.gen_embed_orange("Error", "Internal Error Occured"))
+
+    # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    #                          Creating an lfg
+    @commands.command(name="lfg", help="List all lfg quests", usage="d/p Description")
+    async def lfg(self, ctx, quest_id=None):
+
+        sql = """SELECT * FROM quest WHERE server_id=$1"""
+        try:
+            rows = await self.bot.pool.fetch(sql, ctx.guild.id)
+        except Exception as e:
+            print(e)
+
+        for record in rows:
+            entry = f"{record[2]}: **{record[3]}** - {record[4]}"
+            response = emb.gen_embed_green(f"Quest ID: {record[1]}", f"{entry}")
+            await ctx.send(embed=response)
 
 
 def setup(bot):
