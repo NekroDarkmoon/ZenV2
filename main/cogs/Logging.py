ca9414eec2d9fac2005012b737224e68f91e4ab7
NekroDarkmoon <junaid_maclaren97@hotmail.com>
2020-08-06 03:45:22 -0700

    Changed mod channel names

--- main/cogs/Logging.py
+++ main/cogs/Logging.py
@@ -1,11 +1,11 @@
-
 # --------------------------------------------------------------------------
 #                                 Imports
 # --------------------------------------------------------------------------
 # Standard library imports
+import re
 import sys
 import os
-import re
+
 # Third party imports
 import discord # noqa
 from discord import utils
@@ -22,34 +22,43 @@ from settings import embeds as emb # noqa
 #                                 Main
 # --------------------------------------------------------------------------
 class Logging(commands.Cog):
-
     def __init__(self, bot):
         self.bot = bot
 
+    # Cog Check
+    # async def cog_check(self, ctx):
+    #     pass
+
+    # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    #                               On delete query
     @commands.Cog.listener(name="on_message_delete")
     async def on_message_delete(self, message):
+        # Validation
         regex = "^[^\"\'\.\w]"
+        if re.search(regex, message.content):
+            return
+        if message.author.bot:
+            return
+
+        # Getting Variables
+        author = message.author
+        oc = message.channel
+        content = message.content
+        guild = message.guild
+        attachment = message.attachments
+        if attachment != []:
+            attachment = message.attachments[0].proxy_url
+
+        # Sending to log channel
         try:
-            if re.search(regex, message.content):
-                return
-            if message.author.bot:
-                return
-
-            author = message.author
-            oc = message.channel
-            content = message.content
-            guild = message.guild
-            attachment = message.attachments
-            if attachment != []:
-                attachment = message.attachments[0].proxy_url
-
-            cat = utils.get(guild.categories, name='logs')
+            cat = utils.get(guild.categories, name='Moderation')
             if cat is None:
-                cat = await guild.create_category('logs')
+                cat = await guild.create_category('Moderation')
 
-            send_channel = utils.get(guild.text_channels, name='all-logs')
+            send_channel = utils.get(guild.text_channels, name='modlog')
             if send_channel is None:
-                send_channel = await guild.create_text_channel('all-logs', category=cat)
+                send_channel = await guild.create_text_channel('modlog', category=cat)
+
             # Creating Embed
             response = emb.gen_embed_orange("Deleted Message Log",
                                             f"""Channel: {oc}
@@ -58,33 +67,39 @@ class Logging(commands.Cog):
                                             Attachments: {attachment}""")
 
             await send_channel.send(embed=response)
+
         except Exception as e:
             print(e)
 
+    # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    #                               On edit query
     @commands.Cog.listener(name="on_message_edit")
     async def on_message_edit(self, before, after):
+        # Validation
+        if before.author.bot:
+            return
+        if (after.edited_at - before.created_at).total_seconds() < 60:
+            return
+
+        # Getting Variables
+        author = before.author
+        oc = before.channel
+        oldContent = before.content
+        newContent = after.content
+        guild = after.guild
+        attachment = after.attachments
+        if attachment != []:
+            attachment = after.attachments[0].proxy_url
+
+        # Logging to channel
         try:
-            if before.author.bot:
-                return
-            if (after.edited_at - before.created_at).total_seconds() < 60:
-                return
-
-            author = before.author
-            oc = before.channel
-            oldContent = before.content
-            newContent = after.content
-            guild = after.guild
-            attachment = after.attachments
-            if attachment != []:
-                attachment = after.attachments[0].proxy_url
-
-            cat = utils.get(guild.categories, name='logs')
+            cat = utils.get(guild.categories, name='Moderation')
             if cat is None:
-                cat = await guild.create_category('logs')
+                cat = await guild.create_category('Moderation')
 
-            send_channel = utils.get(guild.text_channels, name='all-logs')
+            send_channel = utils.get(guild.text_channels, name='modlog')
             if send_channel is None:
-                send_channel = await guild.create_text_channel('all-logs', category=cat)
+                send_channel = await guild.create_text_channel('modlog', category=cat)
             # Creating Embed
             response = emb.gen_embed_orange("Edited Message Log",
                                             f"""Channel: {oc}
@@ -97,33 +112,38 @@ class Logging(commands.Cog):
         except Exception as e:
             print(e)
 
+    # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    #                               On update query
     @commands.Cog.listener(name="on_member_update")
     async def on_member_update(self, before, after):
+        # Validation
+        if before.bot:
+            return
+
+        # Get variables
+        member = before.name
+        id = before.id
+        oldNick = before.nick
+        newNick = after.nick
+        guild = before.guild
+
+        if oldNick is None:
+            oldNick = before.name
+        if newNick is None:
+            newNick = before.name
+
+        if oldNick == newNick:
+            return
+
+        #  Logging to channel
         try:
-            if before.bot:
-                return
-
-            member = before.name
-            id = before.id
-            oldNick = before.nick
-            newNick = after.nick
-            guild = before.guild
-
-            if oldNick is None:
-                oldNick = before.name
-            if newNick is None:
-                newNick = before.name
-
-            if oldNick == newNick:
-                return
-
-            cat = utils.get(guild.categories, name='logs')
+            cat = utils.get(guild.categories, name='Moderation')
             if cat is None:
-                cat = await guild.create_category('logs')
+                cat = await guild.create_category('Moderation')
 
-            send_channel = utils.get(guild.text_channels, name='all-logs')
+            send_channel = utils.get(guild.text_channels, name='modlog')
             if send_channel is None:
-                send_channel = await guild.create_text_channel('all-logs', category=cat)
+                send_channel = await guild.create_text_channel('modlog', category=cat)
             # Creating Embed
             response = emb.gen_embed_orange(member,
                                             f"""User Id: {id}
